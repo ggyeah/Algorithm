dcef9f3d4f6527a96e9a41aa5fe1b2663b2dc773
ggyeah <133733223+ggyeah@users.noreply.github.com>
2023-11-07 21:45:09 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.40 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+       	char[] charArray = s.toCharArray();
+        Arrays.sort(charArray);
+        
+        answer = new StringBuilder(new String(charArray)).reverse().toString();
+        
+        return answer;
+    }
+}
